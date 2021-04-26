b551568ab3407dfaef1a1402fdece22218352742
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2021-04-26 21:38:18 +0200

    Install prettier

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,6 +1,6 @@
-import { Greeter } from "../index";
-describe("My Greeter", () => {
-  it("", () => {
-    expect(Greeter("Carl")).toBe("Hello Carl");
-  });
+import { Greeter } from '../index';
+describe('My Greeter', () => {
+    it('', () => {
+        expect(Greeter('Carl')).toBe('Hello Carl');
+    });
 });
